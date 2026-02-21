8bab2db1658003a9e669043cf69e340d79b16cd0
Adnan Saeed <adnansaeed.datascientist@gmail.com>
2026-02-21 15:32:03 +0500

    just get yes-no

---
+++ CONFIG.py
@@ -0,0 +1,16 @@
+OPENAI_MODEL = 'gpt-4o-mini'
+OPENAI_EMBEDDED_MODEL = 'text-embedding-3-small'
+GROQ_MODEL='llama-3.3-70b-versatile'
+GEMINI_MODEL='gemini-2.5-flash-lite'
+DOCKER_MODEL="llama3.2:1B-Q8_0"
+
+BASE_URL="http://localhost:12434"
+
+# Database Configuration
+POSTGRES_USER='POSTGRES'
+POSTGRES_PASSWORD='POSTGRES'
+POSTGRES_DB='POSTGRES'
+
+# ====================== S3 =========================
+# S3 = boto3.client('s3')
+# BUCKET = 's3-rag-buckets'
